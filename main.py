6e137298b5fa4a345eb495ab77ce91de1a4168f3
youtissoum <youtissoum@outlook.fr>
2022-12-14 17:54:51 +0100

    added a command that gives some help about the bot

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands, tasks
 from itertools import cycle
+from BotViews import HelpView
 import os
 from TOKEN import token
 
-client = commands.Bot(command_prefix=None, help_command=None)
+client = commands.Bot(command_prefix="$", help_command=None)
 
 @client.event
 async def on_ready():
@@ -15,5 +16,16 @@ async def on_ready():
 async def change_status():
     statuses = cycle(['the dev suffering', 'everybody else that lost the competition'])
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=next(statuses)))
+
+testing_servers = [911372583042162748, 809824925660479539, 1012804209982308362]
+
+@client.slash_command(description = "Get help about this bot", test_guilds=testing_servers)
+async def help(ctx: discord.Message):
+    view = HelpView()
+    
+    await ctx.respond("sent the help message", ephemeral=True)
+    
+    message = await ctx.send("_ _", view=view)
+    await view.init(message)
 
 client.run(token)
