08a492a4b6ccbd6f89de14eee7badf197158c5da
youtissoum <youtissoum@outlook.fr>
2022-12-15 16:31:10 +0100

    added the echo command

--- main.py
+++ main.py
@@ -37,6 +37,9 @@ async def help(ctx: discord.Message):
     message = await ctx.send("_ _", view=view)
     await view.init(message)
 
+@client.slash_command(description = "Make the bot say anything you want", test_guilds=testing_servers)
+async def echo(ctx, message: str):
+    await ctx.respond(message)
 client.run(token)
 with open('bot_data.json', 'w') as f:
     f.write(json.dumps(bot_data))
